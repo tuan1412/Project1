21be49c0cbd132e8d6382eda2947ceda593659dc
quangbk1909 <nguyenhuyuquangbk1909@gmail.com>
2017-12-05 21:26:52 +0700

    goalController

--- src/app/controller/GoalController.java
+++ src/app/controller/GoalController.java
@@ -3,45 +3,86 @@ package app.controller;
 import java.time.LocalDate;
 
 import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXDialog.DialogTransition;
 
+import app.dao.QueryTimeWork;
+import app.model.TimeWorkOfDay;
 import javafx.animation.RotateTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
 public class GoalController {
 	@FXML
-    private JFXDatePicker dayPicker;
+	private JFXDatePicker dayPicker;
 
-    @FXML
-    private PieChart pieChart;
+	@FXML
+	private PieChart pieChart;
 
-    @FXML
-    private TextField goalFeild;
+	@FXML
+	private TextField goalFeild;
 
-    @FXML
-    private ImageView reload;
-    
-    public void initialize() {
-    	dayPicker.setValue(LocalDate.now());
-    	goalFeild.setText("25");
-    	
-    	reload.setOnMouseClicked(e->reloadChart());
+	@FXML
+	private ImageView reload;
+	
+
+	TimeWorkOfDay timeWork = null;
+	private static int goal = 120;
+
+	public void initialize() {
+		dayPicker.setValue(LocalDate.now());
+		goalFeild.setText(Integer.toString(goal));
+		reload.setOnMouseClicked(e -> reloadChart());
 	}
-    
-    private void reloadChart() {
-    	//viet them ham gay du lieu tu dayPicker va goalFeild
-    	RotateTransition trans = new RotateTransition(Duration.seconds(1.5), reload);
-    	trans.setByAngle(360);
-    	trans.play();
-    	trans.setOnFinished(e->showChart());
-   
-    }
-    //xu ly hien thi o day, neu cam thay bieu do tron k hop thi tuy o chinh.
-    private void showChart() {
-    	
-    }
-    
+
+	private void reloadChart() {
+		// viet them ham gay du lieu tu dayPicker va goalFeild
+		timeWork = new QueryTimeWork().getTimeWorkOfDay(ContextController.getInstance().getLoggedUserId(),
+				dayPicker.getValue().toString());
+		if (timeWork == null) {
+			timeWork = new TimeWorkOfDay(dayPicker.getValue().toString(), 0);
+		}
+		try {
+			goal = Integer.parseInt(goalFeild.getText());
+		} catch (Exception e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText("Error");
+			alert.setContentText("Please fill in goal field number minutes goad of day!!");
+			alert.showAndWait();
+		}
+		RotateTransition trans = new RotateTransition(Duration.seconds(1.5), reload);
+		trans.setByAngle(360);
+		trans.play();
+		trans.setOnFinished(e -> showChart());
+
+	}
+
+	// xu ly hien thi o day, neu cam thay bieu do tron k hop thi tuy o chinh.
+	private void showChart() {
+		float rate = (float) timeWork.getMinutesWork() / goal;
+		ObservableList<Data> list = FXCollections.observableArrayList();
+		if (rate < 1) {// chua hoan thanh goal
+			list = FXCollections.observableArrayList(
+					new PieChart.Data("Completed", rate),
+					new PieChart.Data("Left", 1 - rate));
+					//hien thi label vs truong hop chua hoan thanh goal :  timeWork.getMinutesWork() '/' goal...
+			        
+		} else {// da hoan thanh
+			list = FXCollections.observableArrayList(
+					new PieChart.Data("Completed", rate), 
+					new PieChart.Data("Left", 0));
+					// da hoan thanh goal vs thanh tich timeWork.getMinutesWork() '/' goal. dat goal moi...
+		}
+		pieChart.setData(list);
+
+	}
+
 }
--- src/app/dao/QueryTimeWork.java
+++ src/app/dao/QueryTimeWork.java
@@ -32,4 +32,27 @@ public class QueryTimeWork {
 		return listTime;
 	}
 
+	public TimeWorkOfDay getTimeWorkOfDay(int idUser,String date) {
+		TimeWorkOfDay timeWork = null;
+		try {
+			Connection con = ConnectionUtils.getConnection();
+			String query = "Select * from Statistic where idUser = ? and date = ?";
+			PreparedStatement pst =  con.prepareStatement(query);
+			pst.setInt(1, idUser);
+			pst.setString(2, date);
+			ResultSet rs = pst.executeQuery();
+			if(rs.next()) {
+				timeWork = new TimeWorkOfDay(rs.getString(2), rs.getInt(3));
+			}
+			pst.close();
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return timeWork;
+	}
+//	public static void main(String[] args) {
+//		QueryTimeWork test = new QueryTimeWork();
+//		System.out.println(test.getTimeWorkOfDay(2, "2017-11-08").getDate() + '\t' +  test.getTimeWorkOfDay(2, "2017-11-08").getMinutesWork());
+//	}
 }
